db63f50ac7efa2344591dc5af25faa6917c0fa53
Jefferson Holbrook <holbrook002@gmail.com>
2016-02-21 06:25:34 -0500

    better formatting and impl for http requesting data from our own API

--- frontend/assets/js/map.js
+++ frontend/assets/js/map.js
@@ -13,19 +13,144 @@ var app = angular.module("MapApp", ['ngMaterial']);
 	// 	};
 	// });
 
-app.controller("MapCtrl", function($scope, $mdSidenav){
-	$scope.searchText = "";
+app.controller("MapCtrl", function($scope, $mdSidenav, $log, $mdSidenav, $http, $window){
 
+	$window.map;
+	$window.initMap = function initMap() {
+		var map;
+	    map = new google.maps.Map(document.getElementById('map'), {
+
+	        center: {lat: 40.7127, lng: 74.0059},
+	        zoom: 8
+	    });
+
+	    var geocoder = new google.maps.Geocoder;
+
+
+	    if(navigator.geolocation){
+	    	navigator.geolocation.getCurrentPosition(function(position){
+					initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+					map.setCenter(initialLocation);
+
+					geocoder.geocode({'location': map.getCenter()}, function(results, status){
+						if(status === google.maps.GeocoderStatus.OK){
+							if (results[1]){
+								map.setZoom(11);
+								var marker = new google.maps.Marker({
+									position: map.getCenter(),
+									map: map
+								});
+
+								$log.debug(results[1].formatted_address);
+								$scope.searchText=results[1].formatted_address;
+							}
+
+						}
+					})
+				}, function(){
+					//alert that the location service is not available
+				});
+
+		} else {
+			alert("geolocation services not available");
+
+		}
+	}	
+	$scope.searchText = "";
+	// $scope.geoLocationAvailable = navigator.geolocation;
+	// if($scope.geoLocationAvailable){
+	// 	$log.debug('Geolocation is available');
+	// } else {
+	// 	$log.debug('Geolocation NOT available');
+	// }
+
+	$scope.repositionMap = function(position){
+
+		if(navigator.geolocation){
+			var startPos;
+			// 
+		}
+	};
+
+	$scope.listResults = '';
+	$scope.listStatus;
 	
 
+	$scope.search = function(useCurrentLocation){
+		var geocoder;
+		var initialLocation;
+		$scope.url;
+		// map = document.getElementById("map");
+		var PP_url = "https://www.parkingpanda.com/api/v2/locations?search=";
+		
+		// var geoSuccess = function(position){
+		// 	$log.debug(position);
+		// 	//geocoder.geocode( {'location': position}, )
+		// 	url = PP_url.concat(position);
+		// };
+		if(useCurrentLocation){
 
+			url = PP_url.concat();
+		} else {
+			url = PP_url.concat($scope.searchText);
+		}
+		url = "https://pcf-2016.appspot.com/api/lots";
+		$log.debug(url);
+		$http({
+			method: 'GET',
+			url: url
+		}).then(function(response){
+			$log.debug(response.status);
+			$log.debug(response.data);
+
+			$scope.listResults = response.data;
+			$scope.openRightSidenav();
+
+			//add markers to the map
+			// for(var i = 0; i < response.data.length; i++){
+			// 	var location = response.data[i];
+			// 	var marker = new google.maps.Marker({
+			// 		position: {lat: location.Lat, lng: location.Lng},
+			// 		map: map,
+			// 		title: location.title
+			// 	});
+			// }
+
+		}, function(response){
+			$log.debug(response.data || "Request Failed");
+			$log.debug(response.status);
+		});
+
+	};
+
+	$scope.openRightSidenav = function(){
+		if($mdSidenav('right-sidenav').isOpen()){
+			$mdSidenav('right-sidenav').close();
+		} else {
+			$mdSidenav('right-sidenav').open();
+		}
+	}
  });
 
-function initMap() {
-	var map;
-    map = new google.maps.Map(document.getElementById('map'), {
+// function initMap() {
+// 	var map;
+//     map = new google.maps.Map(document.getElementById('map'), {
 
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-    });
-}
+//         center: {lat: -34.397, lng: 150.644},
+//         zoom: 8
+//     });
+
+//     if(navigator.geolocation){
+//     	navigator.geolocation.getCurrentPosition(function(position){
+// 				initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+// 				map.setCenter(initialLocation);
+
+// 			}, function(){
+// 				//alert that the location service is not available
+// 			});
+
+// 	} else {
+// 		alert("geolocation services not available");
+
+// 	}
+// }
